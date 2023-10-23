8bbcc9bce5a5d3db40a0bad9b503e4bb78fb2f39
ersatzphrodite <147988769+ersatzphrodite@users.noreply.github.com>
2023-10-23 18:17:40 +0800

    Update BasicIteration_v5.py
    FINAL OUTPUT.

--- BasicIteration_v5.py
+++ BasicIteration_v5.py
@@ -8,7 +8,7 @@ max_num = 7  # Set the maximum number of inputs
 for i in range(max_num):  # Only ask for up to 7 inputs.
     num = input(f"✔ Enter an integer (or press Enter to finish): ")
 #   num = int(input(f"✔ "))
-#       Changed "Enter number:" to check mark (✔) to avoid repetition of print statement.
+    # Changed "Enter number:" to check mark (✔) to avoid repetition of print statement.
 
     if not num:  # If the user presses Enter without entering a number
         break  # Exit the loop
@@ -37,20 +37,20 @@ pairs = []
 for i in range(len(numbers)):
     for j in range(i + 1, len(numbers)):
         if numbers[i] + numbers[j] == 0:
-            #   pair_found = True (`if not pair_found` is no longer in use so this serves no purpose.)
-            #   break   (Removed statement after `pair_found` to continue checking for other pairs.)
+            # pair_found = True - `if not pair_found` is no longer in use so this serves no purpose.
+            # break - Removed statement after `pair_found` to continue checking for other pairs.
             pairs.append((numbers[i], numbers[j]))
-            #   Used pairs.append to store pairs in a list instead of multiple line output in my initial submission.
+            # Used pairs.append to store pairs in a list instead of multiple line output in my initial submission.
 #       if pair_found: - also irrelevant
 #           continue - also irrelevant
 
-#   if not pair_found: (Checks the boolean flag `pair_found` which is no longer in use.)
-    #   print("No pair has the sum of 0.")
-        #   Figure out how to remove loop in output if there are no pairs.
-        #   Solution: moved `if not pair_found` outside the inner loop.
+#   if not pair_found: - Checks the boolean flag `pair_found` which is no longer in use.
+#       print("No pair has the sum of 0.")
+            # Figure out how to remove loop in output if there are no pairs.
+            # Solution: moved `if not pair_found` outside the inner loop.
 
-#   if pairs: (Checks if the pairs list is not empty. Easier to understand than `if not pair_found`.)
-    #   print("Pairs found:", pairs)    # Executes if pairs list = True (not empty).
+#   if pairs: - Checks if the pairs list is not empty. Easier to understand than `if not pair_found`.
+#       print("Pairs found:", pairs) - Executes if pairs list = True (not empty).
 
 if len(pairs) == 1:  # Checks if there's exactly one pair in the pairs list.
     print("✔ The pair", pairs[0], "has a sum of zero.")
@@ -60,3 +60,4 @@ else:
     print("✔ No existing pair has a sum of zero.")  # Executes if pairs list = False (empty).
 
 print("▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩▩")
+
