f82c78ed46a69c451407b78572152f6affbb9ef3
ersatzphrodite <147988769+ersatzphrodite@users.noreply.github.com>
2023-10-23 13:36:49 +0800

    Update BasicIteration_v2.1.py

--- BasicIteration_v2.1.py
+++ BasicIteration_v2.1.py
@@ -15,19 +15,19 @@ pairs = []
 for i in range(len(numbers)):
     for j in range(i + 1, len(numbers)):
         if numbers[i] + numbers[j] == 0:
-            #   pair_found = True
+            #   pair_found = True (if not pair_found is no longer used so this serves no purpose.)
             #   break   (Removed statement after the "Pair found" message to continue checking for other pairs.)
             pairs.append((numbers[i], numbers[j]))
             #   Used pairs.append to store pairs in a list instead of multiple line output in my initial submission.
         if pair_found:
             continue
 
-#   if not pair_found:
+#   if not pair_found: (Checks the boolean flag pair_found which is no longer in use.)
     #   print("No pair has the sum of 0.")
         #   Figure out how to remove loop in output if there are no pairs.
         #   Solution: moved if not pair_found outside the inner loop.
 
-if pairs:  # Checks if the pairs list is not empty.
+if pairs:   # Checks if the pairs list is not empty. Works better than if not pair_found.
     print("Pairs found:", pairs)    # Executes if pairs list = True (not empty).
 else:
     print("No pair has the sum of 0.")  # Executes if pairs list = False (empty).
